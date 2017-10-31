360d24832f33a88900386f5d6a05d82602a1752a
Joshua D Doll <jdoll@dragos.com>
2017-10-31 12:08:52 -0400

    cleanup acceptance tests

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,11 +1,14 @@
 require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
 
-$module_path = '/etc/puppetlabs/code/modules/'
+UNSUPPORTED_PLATFORMS = %w([windows] [Darwin]).freeze
 
-# Install Puppet on all hosts
-install_puppet_agent_on(hosts, {
-  :puppet_agent_version          => '1.9.1',
-})
+run_puppet_install_helper
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ %r{pe}i
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
+
 
 RSpec.configure do |c|
   module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
@@ -15,12 +18,16 @@ RSpec.configure do |c|
   c.before :suite do
     # Sync modules to all hosts
     hosts.each do |host|
-      if fact('osfamily') == 'RedHat'
-        on host, 'yum install -y rsync'
+      if fact_on(host, 'osfamily') == 'Debian'
+        on host, 'apt-get install -y unzip'
+      elsif fact_on(host, 'osfamily') == 'RedHat'
+        on host, 'yum install -y unzip'
       end
-      logger.notify "ensure rsync exists on #{host}"
-      rsync_to(host, "#{module_root}/spec/fixtures/modules", $module_path, {})
-      install_dev_puppet_module_on(host, :source => module_root, :module_name => 'vault_client', :target_module_path => $module_path)
+      on host, 'iptables -F INPUT'
+      on host, 'ln -sf /etc/puppetlabs/code/modules/vault_client/files/vault-dev-server.service /etc/systemd/system/vault-dev-server.service'
+      on host, 'systemctl daemon-reload'
+      on host, 'systemctl start vault-dev-server.service'
+      copy_module_to(host, source: module_root, module_name: 'vault_client')
     end
   end
 end
